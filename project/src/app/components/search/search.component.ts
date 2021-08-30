bce24d16047fa602f6de54a7c79fa9916be501c3
Petrov012110 <grisha14.06.96@gmail.com>
2021-08-30 23:03:18 +0500

    react form

--- project/src/app/components/search/search.component.ts
+++ project/src/app/components/search/search.component.ts
@@ -10,6 +10,7 @@ import { ResourceService } from '../../services/resourses.service';
 export class SearchComponent implements OnInit {
   text: string = "";
   inputForm: FormGroup;
+  arrData: object[] = []
 
 
   constructor(private _resours: ResourceService) { 
@@ -24,14 +25,28 @@ export class SearchComponent implements OnInit {
     
   }
 
+  public writeData(obj: object) {
+    this.arrData.push(obj)
+  }
+
   public getData(): void {
 
     console.log(this.inputForm.controls['inputControl'].value);
     
+    let obj: object[] = [];
     
-    this._resours.getTwitchData(this.inputForm.controls['inputControl'].value).subscribe(el => console.log("Twitch", el));
-    this._resours.getGitData(this.inputForm.controls['inputControl'].value).subscribe(el => console.log("Git", el));
-    this._resours.getWikiData(this.inputForm.controls['inputControl'].value).subscribe(el => console.log("Wiki", el))
+    this._resours.getTwitchData(this.inputForm.controls['inputControl'].value).subscribe(el => el.forEach((element: object) => {
+      obj.push(element)
+    }));
+    this._resours.getGitData(this.inputForm.controls['inputControl'].value).subscribe(el => el.forEach((element: object) => {
+      obj.push(element)
+    }));
+    this._resours.getWikiData(this.inputForm.controls['inputControl'].value).subscribe(el => el.forEach((element: object) => {
+      obj.push(element)
+    }));
+
+    console.log(obj);
+    
   }
 
   public createArr(text: string): string[] {
